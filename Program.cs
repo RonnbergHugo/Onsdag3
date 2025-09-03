f87e94faaf905551bf885628eadcbadd98ce68b7
RonnbergHugo <hugo.ronnberg@student.nbi-handelsakademin.se>
2025-09-03 11:34:00 +0200

    Added duck

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("  __\r\n<(o )___\r\n ( ._> /\r\n  `---'  ");
         }
     }
 }
